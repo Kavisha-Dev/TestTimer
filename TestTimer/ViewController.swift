998889402b792c6adef81a5a1bf9034d078bc7ac
kavisha <kavisha_jg@yahoo.com>
2020-03-13 22:51:43 +0530

    Made changes

--- TestTimer/ViewController.swift
+++ TestTimer/ViewController.swift
@@ -13,6 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        testTimer()
+    }
+    
+    func testTimer() {
+        print("START - \(getDDMMYYYYHHmmss(Date()))")
+        Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(onRestoreTimerUp),userInfo: nil, repeats: false)
+        print("AFTER TIMER CALLED - \(getDDMMYYYYHHmmss(Date()))")
+        
+        processAddition()
+    }
+    
+    @objc func onRestoreTimerUp() {
+        print("Timer up at \(getDDMMYYYYHHmmss(Date()))")
+    }
+    
+    func processAddition() {
+        print("processAddition CALLED - \(getDDMMYYYYHHmmss(Date()))")
+        let a = 1
+        let b = 2
+        let c = a + b
+        print("Calculated c at \(getDDMMYYYYHHmmss(Date()))")
+    }
+    
+    func getDDMMYYYYHHmmss(_ date: Date? = nil) -> String {
+        guard let date = date else {return ""}
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        return dateFormatter.string(from: date as Date)
     }
 
 
